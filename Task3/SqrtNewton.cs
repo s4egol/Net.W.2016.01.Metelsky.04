2e6bb2328f447f59f7ad5e62ca89eef99d91795d
s4egol <s4egol@DESKTOP-6D3H9BK>
2016-10-30 03:41:46 +0300

    Edited types

--- Task3/SqrtNewton.cs
+++ Task3/SqrtNewton.cs
@@ -8,22 +8,22 @@ namespace Task3
 {
     public static class SqrtNewton
     {
-        public static double SqrtN(int n, int A, double eps = 0.00001)
+        public static double SqrtN(int n, double number, double eps = 0.00001)
         {
-            if ((A < 0) && (n % 2 == 0))
+            if ((number < 0) && (n % 2 == 0))
                 throw new ArgumentException();
             if (n < 2)
                 throw new ArgumentException();
-            if ((A == 0) && (n >= 2))
+            if ((number == 0) && (n >= 2))
                 return 0;
 
-            double x0 = (double)A / (double)n;
-            double x1 = (1 / (double)n) * ((n - 1) * x0 + (double)A / Math.Pow(x0, n - 1));
+            double x0 = number / n;
+            double x1 = (1.0 / n) * ((n - 1) * x0 + number / Math.Pow(x0, n - 1));
 
             while (Math.Abs(x1 - x0) > eps)
             {
                 x0 = x1;
-                x1 = (1 / (double)n) * ((n - 1) * x0 + A / Math.Pow(x0, n - 1));
+                x1 = (1.0 / n) * ((n - 1) * x0 + number / Math.Pow(x0, n - 1));
             }
 
             return Math.Round(x1, 2);
